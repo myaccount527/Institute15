3fec55180c9b71e04d47cb8635553b7bd78bb93e
myaccount527 <mughalinstitute1@gmail.com>
2025-06-15 21:45:02 +0500

    Create server.js

--- server.js
+++ server.js
@@ -50,3 +50,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+app.use(express.static("public"));
+// Route for root path to serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
